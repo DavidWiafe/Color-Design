8846a3ad6ec63628b2503f9cac296a942e9de600
DavidWiafe <wiafe.d@hotmail.com>
2019-05-26 18:59:47 -0400

    first commit

---
+++ ColorDesigne/js/main.js
@@ -0,0 +1,17 @@
+﻿// Your code here!
+/*for (var element of window.document.getElementsByTagName("*")) {
+    element.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}*/
+
+var H1 = document.querySelector("h1");
+var letters = '0123456789ABCDEF';
+H1.textContent = [Math.floor(18.2)];
